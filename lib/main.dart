f66355d7d49aadd80fe1b4f373a797c3ffb1f2b7
Sharon Yelenik <sharon.yelenik@cloudinary.com>
2023-01-08 17:17:09 +0200

    change img size

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:cloudinary_dart/cloudinary.dart';
+import 'package:cloudinary_dart/transformation/transformation.dart';
+import 'package:flutter/material.dart';
+import 'package:cloudinary_dart/transformation/effect/effect.dart';
+import 'package:cloudinary_dart/transformation/resize/resize.dart';
+
+var cloudinary=Cloudinary.fromStringUrl('cloudinary://373364719177799:aa9f1c5a3d4c6ddd85d7c5f7673c4500@demo');
+
+void main() async{
+  cloudinary.config.urlConfig.secure = true;
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: SizedBox(
+              width: 200,
+              height: 140,
+              child: Image.network(cloud()),
+              ),
+          ),
+        ),
+      );
+  }
+}
+
+cloud() {
+  String url = (cloudinary.image('docs/models')
+    ..transformation(Transformation()
+      ..resize(Resize.fill()
+        ..width(250)
+        ..height(250))
+      ..effect(Effect.sepia())))
+       .toString();
+  return url;
+}
